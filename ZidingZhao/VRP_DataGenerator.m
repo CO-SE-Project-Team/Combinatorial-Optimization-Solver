85b74a8b0e31035098b3244021109d660e4cc76e
ZZDDD <ZZDDD2001@gmail.com>
2022-04-05 19:33:10 +0800

    Update 1st Prototype

--- ZidingZhao/VRP_DataGenerator.m
+++ ZidingZhao/VRP_DataGenerator.m
@@ -1,5 +1,5 @@
-VRP_Data.n = int8(20);
-VRP_Data.Capacity = int8(20);
+VRP_Data.n = int64(20);
+VRP_Data.Capacity = int64(20);
 
 VRP_Data.Dmd = int64(randi(10,1,VRP_Data.n));
 VRP_Data.Dmd(1) = 0; % this line can be ignored due to the math model
@@ -17,5 +17,5 @@ VRP_Data.yc = int64(randi(20,1,VRP_Data.n));
 %     VRP_Data.yc(i) = int64(VRP_Data.yc(i));
 % end
 
-VRP_Data.TimeLimit = int8(20);
+VRP_Data.TimeLimit = int64(10);
 save VRP_Data
