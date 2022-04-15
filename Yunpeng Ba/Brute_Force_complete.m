615747a8cd45f76fa8dd6c04bc43207d7d8da614
Yunpeng Ba <12010803@mail.sustech.edu.cn>
2022-04-15 21:52:35 +0800

    final 3
    all ok with correct xi&xj

--- Yunpeng Ba/Brute_Force_complete.m
+++ Yunpeng Ba/Brute_Force_complete.m
@@ -1,22 +1,14 @@
 clc;
 close all;
 clear;
-timeLimit=0.001;
+timeLimit=0.005;
 t1=clock;
 problem='TSP';
-coord=[1 1;2 3;4 5];
+coord=[1 1;2 3;4 5;5 6;6 7;7 8];
 n=size(coord,1);
 %假设默认输入n行2列坐标矩阵coord
 cx=coord(:,1).';
 cy=coord(:,2).';
-xi=[];
-xj=[];
-for k=1:n
-    for m=1:n
-        xi=[xi k];
-        xj=[xj k];
-    end
-end
 
 dis=zeros(n);   % 初始化两个城市的距离矩阵全为0
 for i=2:n    %i从2开始，是因为他与他自己的距离是0
@@ -25,9 +17,11 @@ for i=2:n    %i从2开始，是因为他与他自己的距离是0
     end
 end
 dis = dis+dis';   % 生成对称完整的距离矩阵
-n1=1:n;
-V2=perms(n1);                                %得到全排列遍历矩阵
-V2=[V2,V2(:,1)];                            %将第一列放到矩阵最后一列形成环路
+V2=randperm(n-1);                                %得到全排列遍历矩阵
+for z=1:n-1
+    V2(1,z)=V2(1,z)+1;
+end
+V2=[1 V2 1];
 V=[];
 for k=1:size(V2,1)
     if V2(k,1)==1
@@ -51,6 +45,13 @@ if t>timeLimit
     path_min=-1;
     d_min=-1;
     %-1表示程序没跑完
+end
+if d_min==-1
+    xi=-1;
+    xj=-1;
+else
+    xi=path_min(1,1:n);
+    xj=path_min(1,2:n+1);
 end
 Problem.problem=problem;
 Problem.n=n;
--- Yunpeng Ba/DP_complete.m
+++ Yunpeng Ba/DP_complete.m
@@ -1,19 +1,13 @@
-timeLimit=0.01;
+timeLimit=1;
 t1=clock;
 problem='TSP';
-coord=[1 1;2 3;4 5];
+coord=[1 3;2 8;4 6;15,13;13,1;40,1];
 n=size(coord,1);
 %假设默认输入n行2列坐标矩阵coord
 cx=coord(:,1).';
 cy=coord(:,2).';
 xi=[];
 xj=[];
-for k=1:n
-    for m=1:n
-        xi=[xi k];
-        xj=[xj k];
-    end
-end
 
 dis=zeros(n);   % 初始化两个城市的距离矩阵全为0
 for i=2:n    %i从2开始，是因为他与他自己的距离是0
@@ -28,6 +22,7 @@ dis = dis+dis';   % 生成对称完整的距离矩阵
 [m,n] = size(dis); %dp表，n行, stateNum列
 stateNum = bitshift(1,n-1); %dp表列数为C（n-1）1+...+C（n-1）（n-1）+空集的1
 dp = zeros(n,stateNum); 
+path={};
 % 提前循环初始化为inf
 for i=1:n
     for j=1:stateNum
@@ -39,6 +34,7 @@ end
 % 更新第一列
 for i=1:n
     dp(i,1)=dis(i,1);
+    path(i,1)={1};
 end
 
 %dp（i,j）中的i是一个二进制形式的数，表示经过城市的集合，如0111表示经过了城市0,1,2
@@ -56,12 +52,17 @@ for j=1:stateNum-1
             %因为j就代表城市子集V[j],((j >> (i - 1))是把第i号城市取出来，位与上1等于0说明是从i号城市出发，经过城市子集V[j]，回到起点0号城市
             %“取出来”指集合j里面没有i这个城市，所以可以表示从i触发经过集合j中的城市
             
+            
             for k=0:n-1
-                temp2 = bitand(bitshift(j,-(k-1)),1);
                 if k==0 || bitand(bitshift(j,-(k-1)),1)==1 
-                %如果j的第k位为1，i.e.j中有k这个城市
+                %如果j的第k位为1，即j中有k这个城市
                     
-                    dp(i+1,j+1) = min(dp(i+1,j+1),dis(k+1,i+1)+dp(k+1,bitxor(j,(bitshift(1,k-1)))+1));
+                    if dp(i+1,j+1)>dis(k+1,i+1)+dp(k+1,bitxor(j,(bitshift(1,k-1)))+1)
+                        dp(i+1,j+1) = dis(k+1,i+1)+dp(k+1,bitxor(j,(bitshift(1,k-1)))+1);
+                        path(i+1,j+1)= {[k+1 path(k+1,bitxor(j,(bitshift(1,k-1)))+1)]};
+                    end
+                        
+                        
                     %dp[k][j ^ (1 << (k - 1))，是将dp定位到，从k城市出发，经过城市子集V[s]，回到0号城市所花费的最小距离
                     %如果从k城市出发经过城市子集V[s]，V[s]中肯定不包含k，则在j中把第k个城市置0即可
                     %(j ^ (1 <<k -1))表示j的k位（必为1）置0，其他位保持不变
@@ -77,6 +78,15 @@ end
 
 %% 结果就是
 objVal=dp(1,stateNum);
+route=[];
+a=path{1,stateNum};
+for z=1:n-1
+    route=[route a{1,1}];
+    a=a{1,2};
+end
+route=[1 route 1];
+xi=route(1,1:n);
+xj=route(1,2:n+1);
 t2=clock;
 t=etime(t2,t1);
 if t>timeLimit
