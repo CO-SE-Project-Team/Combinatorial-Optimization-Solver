98abbe8e3c870c4741dfd0c8c6e97899baf51b67
ZZDDD <ZZDDD2001@gmail.com>
2022-05-01 23:58:35 +0800

    Update

--- ZidingZhao/VRP_DataGenerator_v2_matlab.m
+++ ZidingZhao/VRP_DataGenerator_v2_matlab.m
@@ -1,5 +1,5 @@
 Data.problem = 'VRP';
-Data.n = int64(20);
+Data.n = int64(5);
 Data.capacity = int64(20);
 
 Data.demand = int64(randi(10,1,Data.n));
@@ -8,12 +8,12 @@ Data.demand(1) = 0; % this line can be ignored due to the math model
 %     Data.Dmd(i) = int64(Data.Dmd(i));
 % end
 
-Data.cx = int64(randi(20,1,Data.n));
+Data.cx = randi(20,1,Data.n);
 % for i = 1:length(Data.xc)
 %     Data.xc(i) = int64(Data.xc(i));
 % end
 
-Data.cy = int64(randi(20,1,Data.n));
+Data.cy = randi(20,1,Data.n);
 % for i = 1:length(Data.yc)
 %     Data.yc(i) = int64(Data.yc(i));
 % end
---
+++ ZidingZhao/VRP_DataGenerator_v2_python.m
@@ -0,0 +1,29 @@
+Data.problem = 'VRP';
+Data.n = int64(5);
+Data.capacity = int64(20);
+
+Data.demand = int64(randi(10,1,Data.n));
+Data.demand(1) = 0; % this line can be ignored due to the math model
+% for i = 1:length(Data.Dmd)
+%     Data.Dmd(i) = int64(Data.Dmd(i));
+% end
+
+Data.cx = int64(randi(20,1,Data.n));
+% for i = 1:length(Data.xc)
+%     Data.xc(i) = int64(Data.xc(i));
+% end
+
+Data.cy = int64(randi(20,1,Data.n));
+% for i = 1:length(Data.yc)
+%     Data.yc(i) = int64(Data.yc(i));
+% end
+
+Data.iterations = int64(100);
+Data.timeLim = int64(10);
+
+Data.distance = [];
+Data.xi = [];
+Data.xj = [];
+Data.objVal = int64(0);
+
+save VRP_Data_Python_v2
