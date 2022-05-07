b1fcdc6d605a37514131b328c22833fc518dc21b
ZZDDD <ZZDDD2001@gmail.com>
2022-05-07 19:39:49 +0800

    update

--- CO_SOLVER_V1/KP_GREEDY.m
+++ CO_SOLVER_V1/KP_GREEDY.m
@@ -1,83 +1,77 @@
-classdef TSP_GREEDY < ALGORITHM %类名改成 问题_算法, 如把subALGORITHM改成：'TSP_GA'，这个文件需要把ALGORITHM加到目录
-    % SubALGORITHM 继承了 ALGORITHM
-    % 父类具有 Data 结构体, 此处无需再次声明、
-    % 本类不需要有任何变量
+classdef KP_GREEDY < ALGORITHM
     methods
-        % 本类有且仅有以下一种方法/函数
         function solve(obj)
-            % 把父类Data中变量赋予该类中你需要使用的变量， 不同问题可能不同
-            % 如;
-            problem = obj.Data.problem;
-            n = obj.Data.n;
-            capacity = obj.Data.capacity;
-            demand = obj.Data.demand;
-            cx = obj.Data.cx;
-            cy = obj.Data.cy;
-            xi = obj.Data.xi;
-            xj = obj.Data.xj;
-            objVal = obj.Data.objVal;
-
-            % 其他变量设置 & 初始化
-
-            % start_clock() 是父类方法，开始计时
             obj.start_clock();
-            % 开始你的迭代循环
-            inputIndex = [cx.' , cy.'];
-            inputAdjacencyMatrix = zeros(n);
-            for i = 1:n
-                for j = 1:n
-                    inputAdjacencyMatrix(i, j) = sqrt((inputIndex(i, 1) - inputIndex(j, 1)) ^ 2 + (inputIndex(i, 2) - inputIndex(j, 2)) ^ 2);
-                end
-            end
-            row = size(inputAdjacencyMatrix, 1);
-            outputRoad = [];%输出路径
-            outputCost = 0;%输出总长度
-            havePass = zeros(1, row);%标记点是否被走过
-            nextPoint = 1;%确定开始点
-            haveNumber = 0;%确定已走路程
-            while haveNumber < row
-                havePass(1, nextPoint) = 1;
-                outputRoad = [outputRoad, nextPoint];
-                min = 0;
-                haveNumber = haveNumber + 1;
-                for i = 1:row%确定起始搜索点
-                    if havePass(1, i) == 0
-                        min = inputAdjacencyMatrix(nextPoint, i);
-                        break
-                    end
-                end
-                tem = nextPoint;
-                for i = 1:row%找到最短的路径
-                    if havePass(1, i) == 0 && inputAdjacencyMatrix(tem, i) <= min
-                        min = inputAdjacencyMatrix(tem, i);
-                        nextPoint = i;
-                    end
-                end
-                outputCost = outputCost + min;
-            end
-            objVal = 0;
-            
-            outputRoad = [outputRoad , 1];
-            outputCost = outputCost + inputAdjacencyMatrix(row, 1);
+            timeLim=obj.Data.timeLim;
+            iterations=obj.Data.iterations; %迭代次数（暴力用不着）
+            iterator=obj.Data.iterator; %迭代次数（暴力用不着）
+            problem='KP';
 
-            obj.Data.problem = problem;
-            xi = outputRoad(1, 1:n);
-            xj = outputRoad(1 , 2:n + 1);
-            obj.Data.dis=inputAdjacencyMatrix;
+            capacity=obj.Data.capacity;% 背包的容量
+            weight=obj.Data.demand;% 物品的重量
+            cx=obj.Data.cx;
+            cy=obj.Data.cy;% 物品价值
+            n =length(weight);% n为物品的个数
+            objVal=0;%被选择物品的总价值
+            xi=[];
+            xj=[];
+            a=[];%物品单位重量的价值
+            v=[];
+            temp=capacity;
+          
+
+            %计算物品单位重量的价值并降序排列
+            for i=1:n
+                a(i)=cy(i)/weight(i);
+            end
+            [~,place]=sort(a,"descend");
             
-            obj.Data.cx = inputIndex(:, 1).';
-            obj.Data.cy = inputIndex(:, 2).';
-            
-            obj.Data.objVal = outputCost;
-            
+            %在满足选取物品总重量不超过背包容量的前提下，优先选择单位重量价值较大的物品
+            for i=1:n-1
+                objVal=objVal+cy(place(i));
+                capacity=capacity-weight(place(i));
+                if(capacity<0)
+                    objVal=objVal-cy(place(i));
+                    capacity=capacity+weight(place(i));
+                else
+                    v(place(i))=1;
+                end
+            end
+
+            if obj.is_stop()==true
+                objVal=-1;
+                %-1表示程序没跑完
+            end
+            for i=1:n
+                if v(i)==1
+                    xi=[xi,cx(i)];
+                    xj=[xj,cx(i)];
+                end
+            end
+            l=length(xi);
+            if(l>1)
+                xi(l)=[];
+                xj(1)=[];
+            end
+
+            obj.Data.problem=problem;
+            obj.Data.n=n;
+            obj.Data.capacity=temp;
+            obj.Data.demand=weight;
+            obj.Data.cx=cx;
+            obj.Data.cy=cy;
+            obj.Data.dis=0;
             obj.Data.xi=xi;
             obj.Data.xj=xj;
-
-            obj.update_status_by(obj.Data.objVal,obj.Data.xi,obj.Data.xj); % 这将会把当前的objVal，xi，xj更新到GUI中。
+            obj.Data.objVal=objVal;
+            obj.Data.timeLim=timeLim;
+            obj.Data.iterations=iterations;
+            obj.Data.iterator=iterator;
+            
+            obj.update_status_by(obj.Data.objVal,obj.Data.xi,obj.Data.xj);
         end
     end
 end
-
 
 %本地命令行测试步骤
 % p=SubALGORITHM()
