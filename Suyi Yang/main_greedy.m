70d6e8a65d3e145922ae4d44cb4155b32806d3e4
Kagaho0715 <12012441@mail.sustech.edu.cn>
2022-05-18 17:09:12 +0800

    VRP_GREEDY

--- Suyi Yang/main_greedy.m
+++ Suyi Yang/main_greedy.m
@@ -1,37 +1,80 @@
 clear;clc;
-% simple sample data
-% points = [46,42;37,52;52,64;52,33;52,41;62,42;42,57];
-% demand = [0,19,16,11,15,8,8];
-filename= 'db/tests.csv';
-[~,~,file_data] = xlsread( filename);
-data = cell2mat(file_data);
-demand = data(:,1)';
-points = data(:,2:3);
 
-global CantP CantV CV PR D DT MDist
-PR = points;                               % Coordinates
+demand = [0 10 2 10 10];
+cx=[10 17 3 9 19];
+cy=[16 20 14 1 17];
 D = demand;                                % Demands
-CantP=length(D);                           % Total of points
-CV = 40;                                   % Vehicle max capacity
-DT = sum(D);                               % Total demand
-CantV=round(DT/CV)+round(CantP*0.05);      % Vehicle quantity approximate
-[MDist, ~]= dist(PR);                      % Distance matrix. (Euclidean)
+n=length(D);                           % Total of points
+points=[];
+for i=1:n
+    points(i,1)=cx(i);
+    points(i,2)=cy(i);
+end
 
-unvisited = 2:CantP;                       % Points not visited. All in the first time except depot
-vehicles = zeros(CantV,CantP);
+PR = points;                               % Coordinates
+
+capacity = 20;                                   % Vehicle max capacity
+DT = sum(D);                               % Total demand
+CantV=round(DT/capacity)+round(n*0.05);      % Vehicle quantity approximate
+[n,~]=size(PR);
+MDist=zeros(n,n);
+totalD=0;
+for i=1:n
+    p1=PR(i,:);
+    for j=i+1:n
+        p2=PR(j,:);
+        d=sqrt(((p2(1)-p1(1))^2)+((p2(2)-p1(2))^2));
+        MDist(i,j)=d;
+        totalD=totalD+d;
+        MDist(j,i)=d;
+    end
+end
+
+unvisited = 2:n;                       % Points not visited. All in the first time except depot
+vehicles = zeros(CantV,n);
 demands = [];
 distances = [];
 iter = 1;
-tic
-while ~isempty(unvisited)
-    [vehicle,unvisited,demand,distance] = greedy(unvisited);
+r=[1];
+temp_demad = D(unvisited);
+    point_demand = [unvisited;temp_demad];
+    point_demand = sortrows(point_demand',2);
+for j=1:n-1
+            r=[r,point_demand(j,1)];
+        end
+while ~isempty(unvisited)  
+    temp_demad = D(unvisited);
+    point_demand = [unvisited;temp_demad];
+    point_demand = sortrows(point_demand',2);
+    [cantP,~]=size(point_demand);
+    vehicle = [];
+    demand = 0;
+    distance = 0;
+    i = 1;
+    last = 0;
+    while(i <= cantP && demand + point_demand(i,2)<= capacity)
+        if i == 1
+            distance = distance + MDist(1,point_demand(i,1));
+        else
+            distance = distance + MDist(vehicle(last),point_demand(i,1));
+        end
+        vehicle = [vehicle, point_demand(i,1)];
+        last = last + 1;
+        demand = demand + point_demand(i,2);
+        unvisited = unvisited(unvisited ~= point_demand(i,1));        
+        i=i+1;
+    end
     vehicles(iter,1:length(vehicle))= vehicle;
     iter = iter+1;
     demands = [demands,demand];
     distances = [distances,distance];
+    
 end
 cantRealVehicles = iter -1;
-distanceTotal = sum(distances)
-time = toc;
+objVal = sum(distances)
+r=[r,1]
+xi=r(1:n);
+xj=r(2:n+1);
+
 % % Save to a file the results
 % save(strcat('results/greedy_results'));
