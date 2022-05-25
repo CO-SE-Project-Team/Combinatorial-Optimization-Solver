207e1da1938805628279d37d84ea35a8e921dd76
ZhengZhipeng425 <97509755+ZhengZhipeng425@users.noreply.github.com>
2022-05-25 17:52:55 +0800

    。

--- Zhipeng Zheng/VRP_VNS/VRP_TWOOPT.m
+++ Zhipeng Zheng/VRP_VNS/VRP_TWOOPT.m
@@ -1,4 +1,4 @@
-classdef VRP_VNS < ALGORITHM %类名改成 问题_算法, 如把subALGORITHM改成：'TSP_GA'，这个文件需要把ALGORITHM加到目录
+classdef VRP_TWOOPT < ALGORITHM %类名改成 问题_算法, 如把subALGORITHM改成：'TSP_GA'，这个文件需要把ALGORITHM加到目录
     % SubALGORITHM 继承了 ALGORITHM
     % 父类具有 Data 结构体, 此处无需再次声明、
     % 本类不需要有任何变量
@@ -135,43 +135,24 @@ dist = dist + data(1, before);
 end
 end
 function routes = neighborBy2opt( route )
-
+% 根据一条路径route，采用2opt方式产生其全部邻域解集
 cityQty = max(size(route));
-
+% if cityQty <= 3
+%     disp('cityQty is too small in neighborBy2opt.....');
+% end
 pos = 2 : cityQty;
 changePos = nchoosek(pos, 2);
 rows = size(changePos, 1);
 routes = zeros(rows, cityQty);
 % 依次对两个点进行位置互换，形成新的解
-k = 1;
-for i = 2:cityQty
-    for j = i + 1:cityQty
-        city1 = route(1, i);
-        city2 = route(1, j);
-        part1 = [];
-        if i > 2
-        part1 = route(1, 2:i - 1);
-        end
-        part2 = [];
-        if j > i + 1
-            part2 = route(1, i + 1:j - 1);
-        end
-        part3 = [];
-        if cityQty > j
-            part3 = route(1, j + 1:cityQty);
-        end
-        routes(k, :) = [1 route(1,i) route(1,j) part1 part2 part3];
-        k = k + 1;
-    end
+for i  = 1 : rows
+    city1 = route(changePos(i,1));
+    city2 = route(changePos(i,2));
+    midRoute = route;
+    midRoute(changePos(i,1)) = city2;
+    midRoute(changePos(i,2)) = city1;
+    routes(i,:) = midRoute;
 end
-% for i  = 1 : rows
-%     city1 = route(changePos(i,1));
-%     city2 = route(changePos(i,2));
-%     midRoute = route;
-%     midRoute(changePos(i,1)) = city2;
-%     midRoute(changePos(i,2)) = city1;
-%     routes(i,:) = midRoute;
-% end
 end
 
 %本地命令行测试步骤
